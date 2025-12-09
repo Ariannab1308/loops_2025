d9d70339b801b99ff75a926c72959af88d0ca864
Ariannab1308 <ariannab1308@gmail.com>
2025-12-09 21:01:39 +0000

    loops3

--- 1_loops.py
+++ 1_loops.py
@@ -8,15 +8,18 @@ print(fruits[0])
 print(fruits[1])
 print(fruits[2])
 
+# ----------------------------------------
 for fruit in fruits: # this is a lot shorter --> "for loop" 
     print(fruit)
 # i just worked with loops
 
+# ----------------------------------------
 # Given a list of school subjects:
 subjects = ["Math", "Science", "History", "Art"]
 for subject in subjects:
     print(subject)
 
+# ----------------------------------------
 # print out each subject but stop when you reach "History"
 subjects = ["Math", "Science", "History", "Art"]
 for subject in subjects:
@@ -24,6 +27,7 @@ for subject in subjects:
         break
     print(subject)
 
+# ----------------------------------------
 # skip over "Science and print the rest:
 subjects = ["Math", "Science", "History", "Art"]
 for subject in subjects:
@@ -32,6 +36,7 @@ for subject in subjects:
     continue
     print(subject)
 
+# ----------------------------------------
 # Challenge:
 # Use a for loop and range to print each subject along with its index:
 # Example output: "Subject 0: Math"
@@ -41,6 +46,7 @@ for index in range(len(subjects)):
 # Given:
 numbers = [5, 10, 15, 20]
 
+# ----------------------------------------
 # Challenge:
 # Use a for loop to add all the numbers and print the total.
 total = 0
@@ -54,11 +60,18 @@ for number in numbers:
 # fourth time total = 15 + 15
 # fifth time total = 30 + 20
 
-
+# ----------------------------------------
 new_numbers = list(range(1, 600001))
 # this creates a list of numbers from 1 to 600000
 # Challenge: sum up all the numbers from 1 to 600000
 total = 0
 for new_number in new_numbers: 
     total += new_number
+print("total: " ,total)
+
+# ----------------------------------------
+next_numbers = list(range(5,26))
+total = 0
+for next_number in next_numbers:
+    total += next_number
 print("total: " ,total)
