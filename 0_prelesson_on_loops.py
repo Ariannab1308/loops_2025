475385e0d615c9af542d83436135ae20b5df1692
Ariannab1308 <ariannab1308@gmail.com>
2025-12-09 20:22:04 +0000

    loops

--- 0_prelesson_on_loops.py
+++ 0_prelesson_on_loops.py
@@ -1,2 +1,26 @@
-Loops video  https://www.youtube.com/watch?v=KWgYha0clzw&t=1s&pp=ygUOYnJvIGNvZGUgbG9vcHM%3D
-While loops  
+# Loops video  https://www.youtube.com/watch?v=KWgYha0clzw&t=1s&pp=ygUOYnJvIGNvZGUgbG9vcHM%3D
+# While loops  
+
+# for loops = execute a block of code a fixed nuber of time 
+# You can iterate over a range, string, sequence, etc.
+
+# for x in reversed(range(1, 11)): 
+#     print(x)
+# print("HAPPY NEW YEAR") 
+
+# ----------------------------------------
+# for x in range(1, 11, 3): 
+#     print(x)
+
+#----------------------------------------
+# credit_card = "1234-5678-9101-1121"
+# for x in credit_card:
+#     print(x)
+
+# ----------------------------------------
+for x in range (1, 21):
+    if x == 13:
+        break
+        continue
+    else:
+        print(x)
