b88ad296a35bec2b91508f6a2d7d6e24c662df58
Ariannab1308 <ariannab1308@gmail.com>
2025-12-11 20:10:25 +0000

    loopsss

--- 4_problem_set_1.py
+++ 4_problem_set_1.py
@@ -5,16 +5,21 @@
 
 
 # ### **Problem 1: Print Numbers 1 to 10
-
+list1 = list(range(1,11))
+print(list)
 # Write a program that prints the numbers from **1 to 10**, each on a new line.
-
+for i in list1:
+    print(i)
 
 
 # ### **Problem 2: Sum of Numbers
 
 # Ask the user for a number **n**, then calculate and display the **sum of all numbers from 1 to n**.
 
-
+n = int(inpurt("Enter a number:"))
+total = 0
+for i in range (1,n+1):
+    print("The sum of numbers from 1 to", n, "is:", total)
 
 # ### **Problem 3: Factorial Calculator
 
